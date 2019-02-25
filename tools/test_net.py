3a9792016499e713474507a53f70845b80bb774b
Achal Dave <achalddave@live.com>
2019-02-25 16:25:36 -0500

    Better logging, allow specifying output in test_net.py

--- tools/test_net.py
+++ tools/test_net.py
@@ -6,8 +6,10 @@ from maskrcnn_benchmark.utils.env import setup_environment  # noqa F401 isort:sk
 import argparse
 import logging
 import os
+from pathlib import Path
 
 import torch
+from script_utils.common import common_setup
 from maskrcnn_benchmark.config import cfg
 from maskrcnn_benchmark.data import make_data_loader
 from maskrcnn_benchmark.engine.inference import inference
@@ -28,8 +30,15 @@ def main():
     )
     parser.add_argument(
         '--model-path',
+        type=Path,
         help=('Path to model pickle file. If not specified, the latest '
               'checkpoint, if it exists, or cfg.MODEL.WEIGHT is loaded.'))
+    parser.add_argument(
+        '--output-dir',
+        default='{cfg_OUTPUT_DIR}/inference-{model_stem}',
+        help=('Output directory. Can use variables {cfg_OUTPUT_DIR}, which is '
+              'replaced by cfg.OUTPUT_DIR, and {model_stem}, which is '
+              'replaced by the stem of the file used to load weights.'))
     parser.add_argument("--local_rank", type=int, default=0)
     parser.add_argument(
         "opts",
@@ -53,33 +62,41 @@ def main():
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
     cfg.freeze()
-
-    save_dir = ""
-    logger = logging.getLogger("maskrcnn_benchmark")
-    logger.info("Using {} GPUs".format(num_gpus))
-    logger.info(cfg)
-
-    logger.info("Collecting env info (might take some time)")
-    logger.info("\n" + collect_env_info())
-
     model = build_detection_model(cfg)
     model.to(cfg.MODEL.DEVICE)
 
-    output_dir = cfg.OUTPUT_DIR
-    checkpointer = DetectronCheckpointer(cfg, model, save_dir=output_dir)
+    assert cfg.OUTPUT_DIR, 'cfg.OUTPUT_DIR must not be empty.'
+    checkpointer = DetectronCheckpointer(cfg, model, save_dir=cfg.OUTPUT_DIR)
     if args.model_path:
-        logging.info('Loading model from --model-path: %s', args.model_path)
-        load_path = args.model_path
+        load_path = str(args.model_path.resolve())
+        load_msg = 'Loading model from --model-path: %s' % load_path
     else:
         if checkpointer.has_checkpoint():
             load_path = checkpointer.get_checkpoint_file()
-            logging.info('Loading model from latest checkpoint: %s',
-                        load_path)
+            load_msg = 'Loading model from latest checkpoint: %s' % load_path
         else:
             load_path = cfg.MODEL.WEIGHT
-            logging.info('Loading model from cfg.MODEL.WEIGHT: %s',
-                         load_path)
-    _ = checkpointer.load(load_path, allow_override=False)
+            load_msg = 'Loading model from cfg.MODEL.WEIGHT: %s' % load_path
+
+    output_dir = Path(args.output_dir.format(
+        cfg_OUTPUT_DIR=cfg.OUTPUT_DIR, model_stem=Path(load_path).stem))
+    output_dir.mkdir(exist_ok=True, parents=True)
+    file_logger = common_setup(__file__, output_dir, args)
+    # We can't log the load_msg until we setup the output directory, but we
+    # can't get the output directory until we figure out which model to load.
+    # So we save load_msg and log it here.
+    logging.info(load_msg)
+    logging.info('Output inference results to: %s' % output_dir)
+
+    logger = logging.getLogger("maskrcnn_benchmark")
+    logger.info("Using {} GPUs".format(num_gpus))
+    file_logger.info('Config:')
+    file_logger.info(cfg)
+
+    file_logger.info("Collecting env info (might take some time)")
+    file_logger.info("\n" + collect_env_info())
+
+    checkpointer.load(load_path, allow_override=False)
 
     iou_types = ("bbox",)
     if cfg.MODEL.MASK_ON:
@@ -88,11 +105,10 @@ def main():
         iou_types = iou_types + ("keypoints",)
     output_folders = [None] * len(cfg.DATASETS.TEST)
     dataset_names = cfg.DATASETS.TEST
-    if cfg.OUTPUT_DIR:
-        for idx, dataset_name in enumerate(dataset_names):
-            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference", dataset_name)
-            mkdir(output_folder)
-            output_folders[idx] = output_folder
+    for idx, dataset_name in enumerate(dataset_names):
+        output_folder = output_dir / dataset_name
+        mkdir(output_folder)
+        output_folders[idx] = output_folder
     data_loaders_val = make_data_loader(cfg, is_train=False, is_distributed=distributed)
     for output_folder, dataset_name, data_loader_val in zip(output_folders, dataset_names, data_loaders_val):
         inference(
